5d4597418191e43179e857ab2aa75100c1724747
Jeremy Leonard <jeremyl@gracon.com>
2021-11-04 15:44:53 -0400

    Upload New File

---
+++ install.ps1
@@ -0,0 +1,23 @@
+﻿$DriverName = "HP Universal Printing PS (v7.0.1)";
+
+If ($ENV:PROCESSOR_ARCHITEW6432 -eq "AMD64") {
+    Try {
+        &"$ENV:WINDIR\SysNative\WindowsPowershell\v1.0\PowerShell.exe" -File $PSCOMMANDPATH
+    }
+    Catch {
+        Throw "Failed to start $PSCOMMANDPATH"
+    }
+    Exit
+}
+
+$InfPath = (Split-Path -Parent $MyInvocation.MyCommand.Definition) + "\drivers\*.inf";
+
+$infs = Get-ChildItem -Path $InfPath -Name;
+
+Write-Host "Installing the following .inf files:" $infs;
+
+&C:\Windows\System32\pnputil.exe /add-driver $InfPath
+
+Write-Host "Installing Driver for" $DriverName;
+
+Add-PrinterDriver -Name "$DriverName";
