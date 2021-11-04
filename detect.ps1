bd326486934c4ec176c031d1f6061527b205d054
Jeremy Leonard <jeremyl@gracon.com>
2021-11-04 15:40:19 -0400

    Upload New File

---
+++ detect.ps1
@@ -0,0 +1,9 @@
+﻿$DriverName = "HP Universal Printing PS (v7.0.1)";
+$installed = Get-PrinterDriver -Name "$DriverName" -ErrorAction SilentlyContinue;
+if($installed -eq $null){
+    Write-Host "false";
+    exit 1;
+}else{
+    Write-Host "true";
+    exit 0;
+}
